ffcc748a76417e735ae76e9fd97b277c26ed480e
jpCabezon <118548128+jpCabezon@users.noreply.github.com>
2024-11-11 19:52:47 -0300

    Deploy JSON Test

---
+++ index.js
@@ -0,0 +1,10 @@
+//ejecuta una serie de comandos y se enlaza al almacen 
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("almacen.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 10000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
